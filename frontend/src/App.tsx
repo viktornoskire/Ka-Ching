b8f24ce4225cf7675933b1822156ce5114da8af1
ViktorNoskire <vikkerinho@gmail.com>
2026-01-08 11:43:41 +0100

    Create a simple template of the converter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className={"bg-[#2A2A2A] h-screen"}>
+        <header className={"flex justify-center items-center bg-gray-100"}>
+        <h1 className={"text-4xl font-bold m-2 mr-100"}>Ka-Ching</h1>
+        </header>
+        <div className={"flex justify-center items-center"}>
+            <form className={"w-[35%] h-100 ml-auto mr-auto mt-20 bg-[#cecece]"} action="">
+                <h2 className={"text-2xl font-bold underline mb-10"} >Currency Converter</h2>
+                <div>
+                    <label htmlFor="sourceCurrency">Base Amount</label>
+                    <input type="text"/>
+                    <select>
+                        <option value="EUR">EUR</option>
+                        <option value="USD">USD</option>
+                        <option value="AUD">AUD</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="convertedAmount">Converted amount</label>
+                    <input type="text" className={"convertedAmount"}/>
+                    <select>
+                        <option value="EUR">EUR</option>
+                        <option value="USD">USD</option>
+                        <option value="AUD">AUD</option>
+                    </select>
+                </div>
+            </form>
+        </div>
+            </div>
     </>
   )
 }
