c06d1d84814a489805c495ba13aead56786fde65
ViktorNoskire <vikkerinho@gmail.com>
2026-01-08 14:27:43 +0100

    Save fetched data to a database

--- backend/src/main/java/com/example/backend/scheduler/CurrencyScheduler.java
+++ backend/src/main/java/com/example/backend/scheduler/CurrencyScheduler.java
@@ -1,18 +1,23 @@
 package com.example.backend.scheduler;
 
+import com.example.backend.courier.ExchangeRateService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CurrencyScheduler {
-    // private final ExchangeRateService exchangeRateService;
+     private final ExchangeRateService exchangeRateService;
 
-    // public CurrencyScheduler(ExchangeRateService exchangeRateService) {
-    //     this.exchangeRateService = exchangeRateService;
-    // }
+     public CurrencyScheduler(ExchangeRateService exchangeRateService) {
+         this.exchangeRateService = exchangeRateService;
+     }
 
     @Scheduled(cron = "${scheduler.currency-fetch-cron}")
     public void fetchRates() {
-        // exchangeRateService.updateRates();
+        try {
+            exchangeRateService.updateRates();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Failed to update currency rates:");
+        }
     }
 }
