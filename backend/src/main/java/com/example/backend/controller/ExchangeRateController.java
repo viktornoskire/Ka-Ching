b369b4bd9d2d60135dd93d891d19e28f9bb73b5d
ViktorNoskire <vikkerinho@gmail.com>
2026-01-24 13:56:21 +0100

    Enable an api key to allow external cron job

--- backend/src/main/java/com/example/backend/controller/ExchangeRateController.java
+++ backend/src/main/java/com/example/backend/controller/ExchangeRateController.java
@@ -3,13 +3,15 @@ package com.example.backend.controller;
 import com.example.backend.courier.ExchangeRateClient;
 import com.example.backend.dto.CurrencyResponse;
 import com.example.backend.dto.NameIsoResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/rates")
-@CrossOrigin(origins = "https://ka-ching-3616.onrender.com/")
 public class ExchangeRateController {
 
     private final ExchangeRateClient exchangeRateClient;
@@ -18,16 +20,28 @@ public class ExchangeRateController {
         this.exchangeRateClient = exchangeRateClient;
     }
 
+    @Value("${app.CRON_API_KEY}")
+    private String cronSecret;
+
     @GetMapping("update")
-    public void updateCurrencyRates() throws InterruptedException {
+    public void updateCurrencyRates(
+            @RequestHeader("X-CRON-SECRET") String secret
+    ) throws InterruptedException {
+
+        if (!secret.equals(cronSecret)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+
         exchangeRateClient.updateCurrencyRates();
     }
 
+    @CrossOrigin(origins = "https://ka-ching-3616.onrender.com/")
     @GetMapping
     public List<NameIsoResponse> getCurrencies(){
         return exchangeRateClient.getCurrencies();
     }
 
+    @CrossOrigin(origins = "https://ka-ching-3616.onrender.com/")
     @GetMapping("{id}")
     public CurrencyResponse getRateByIsoCode(@PathVariable String id) {
         return exchangeRateClient.getCurrency(id);
